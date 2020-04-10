853ce0c06861679c0163aa97678c3df77b6b2907
Moussa Ba <moussa10ba@gmail.com>
2020-04-10 22:15:45 +0000

    page de connexion

---
+++ connexion.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="principal">
+    <div class="header">
+    <img src="images/saquiz.png" alt="">
+    <h3> Le plaisir de jouer </h3>
+    </div>
+    <div id="form">
+            <div id="formheader">
+                <img src="images/close.jpg" alt="">
+            <h4> Login Form </h4>
+           
+            </div>
+
+            <p><input type="text" name="login" placeholder="Login" class="login" > </p>
+        <p><input type="text" name="prenom" placeholder="Password" class="password"> </p>
+    <p>
+        <input type="submit" name="connexion" value="Connexion" class="connexion">
+        <div id="lien">
+        <a href="inscriptionjoueur.php">S’inscrire Connexion pour jouer?</a>
+        </div>
+    </p>
+    </form>
+    </div>
+
+</div>
+</body>
+</html>
